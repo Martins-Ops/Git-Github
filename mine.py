d8d4e8595377db81090944dd53517737fbeb7395
oluwafemi <oluwafemimartins1234@gmail.com>
2023-10-25 12:31:33 +0100

    Merge branch 'Adesanya-Oluwafemi-Martins' into main

--- mine.py
+++ mine.py
@@ -1,6 +1,6 @@
 # Define variables
-name = "PUT YOUR NAME"
-email = 'PUT YOUR EMAIL'
+name = "Adesanya Oluwafemi Martins"
+email = 'oluwafemiadesanya2001@gmail.com'
 
 # Print the name and email address
 print("Name:", name)
